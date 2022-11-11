c258724a01ceca0f584775d4db958c3b49bcc882
Matheus Catel <matheus.catel@clarifysistemas.com>
2022-11-11 15:22:23 -0300

    Fixes

--- electro-sound/main.js
+++ electro-sound/main.js
@@ -1,12 +1,17 @@
 //Execute the command play audio
-function playSound(idElementAudio) {
-  document.querySelector(idElementAudio).play();
-}
+function playSound(AudioSelector) {
+  const element = document.querySelector(AudioSelector).play();
 
+  if (element != null && element.localName === "audio"){
+    element.play();
+  }else{
+    alert("Element not found")
+  }
+}
 //Const responsible to store all "key" classes from the button element.
 const keyList = document.querySelectorAll(".key");
 let counter = 0;
-
+const keyboard = document.querySelector(".keyboard");
 //Loop that executes the playSound based on the id of each key.
 for (let counter = 0; counter < keyList.length; counter++) {
   //Const responsible to store all "key" classes in a list.
@@ -15,6 +20,7 @@ for (let counter = 0; counter < keyList.length; counter++) {
   const instrument = key.classList[1];
   //Const responsible to store the "sound_key_sound" id from audio element.
   const idAudio = `#sound_${instrument}`;
+  console.log(instrument);
 
   // Anonymous Function that executes the sound on  key click.
   key.onclick = function () {
@@ -22,16 +28,19 @@ for (let counter = 0; counter < keyList.length; counter++) {
   };
 
   //Anonymous Function that sets a new class in the classList section.
-  key.onkeydown = function (e) {
+  key.onkeydown = function (event) {
     //It will check if the keywords pressed are Space or Enter, if so, the function executes the command.
     //If not, the command will not word and nothing happens.
-    if (e.code === "Space" || e.code === "Enter") {
+    if (event.code === "Space" || event.code === "Enter") {
       key.classList.add("active");
     }
   };
-
+  
   //Anonymous Function that deletes a class in the classList section.
   key.onkeyup = function () {
     key.classList.remove("active");
   };
 }
+
+
+
